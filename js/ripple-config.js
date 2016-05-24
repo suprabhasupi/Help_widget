0092f7220fab78dc2a22656fd0a063e0fbd8e512
suprabhasupi <suprabhasupi@gmail.com>
2016-05-24 23:19:01 +0530

    tab part has compltd

--- js/ripple-config.js
+++ js/ripple-config.js
@@ -168,3 +168,63 @@ var ripplyScott2 = (function() {
 })();
 
 ripplyScott2.init('js-ripple-btn2', 0.75);
+
+
+
+
+var ripplyScott3 = (function() {
+  var circle = document.getElementById('js-ripple3'),
+      ripple = document.querySelectorAll('.js-ripple3');
+
+  function rippleAnimation3(event, timing) {
+    var tl           = new TimelineMax();
+        x            = event.offsetX,
+        y            = event.offsetY,
+        w            = event.target.offsetWidth,
+        h            = event.target.offsetHeight,
+        offsetX      = Math.abs( (w / 2) - x ),
+        offsetY      = Math.abs( (h / 2) - y ),
+        deltaX       = (w / 2) + offsetX,
+        deltaY       = (h / 2) + offsetY,
+        scale_ratio  = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+
+    console.log('x is:' + x);
+    console.log('y is:' + y);
+    console.log('offsetX is:' + offsetX);
+    console.log('offsetY is:' + offsetY);
+    console.log('deltaX is:' + deltaX);
+    console.log('deltaY is:' + deltaY);
+    console.log('width is:' + w);
+    console.log('height is:' + h);
+    console.log('scale ratio is:' + scale_ratio);
+
+    tl.fromTo(ripple, timing, {
+      x: x,
+      y: y,
+      transformOrigin: '50% 50%',
+      scale: 0,
+      opacity: 1,
+      ease: Linear.easeIn
+    },{
+      scale: scale_ratio,
+      opacity: 0
+    });
+
+    return tl;
+  }
+
+  return {
+    init: function(target, timing) {
+      var button = document.getElementById(target);
+
+      button.addEventListener('click', function(event) {
+        rippleAnimation3.call(this, event, timing);
+      });
+    }
+  };
+})();
+
+ripplyScott3.init('js-ripple-btn3', 0.75);
+
+
+
